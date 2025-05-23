25bb964db375ce5917a46c66cc6f116d6bb98ca3
Shreyash-Bankar <156165905+Shreyash-Bankar@users.noreply.github.com>
2025-05-23 15:19:04 +0530

    Update MergSort.java

--- 2. DevideAndConqure/MergSort.java
+++ 2. DevideAndConqure/MergSort.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class MergSort{
-    public static void merg(int arr[],int si,int m,int ei){
+public class MergeSort{
+    public static void merge(int arr[],int si,int m,int ei){
         int i = si;
         int j = m+1;
         int temp[] = new int[ei+1];
@@ -24,18 +24,18 @@ public class MergSort{
         }
     }
     
-    public static void mergSort(int arr[],int si,int ei){
+    public static void mergeSort(int arr[],int si,int ei){
         if(si>=ei){
             return; 
         }
         int mid = (si+ei)/2;
-        mergSort(arr,si,mid);
-        mergSort(arr,mid+1,ei);
-        merg(arr,si,mid,ei);
+        mergeSort(arr,si,mid);
+        mergeSort(arr,mid+1,ei);
+        merge(arr,si,mid,ei);
     }
     public static void main(String args[]){
         int arr [] = {2,4,2,4,8,3,6};
-        mergSort(arr,0,arr.length-1);
+        mergeSort(arr,0,arr.length-1);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
